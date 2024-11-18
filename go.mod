486f33a3751746ddd836967deb84375ee162780c
Tobias <tobias@olsson.be>
2024-11-18 22:22:57 +0100

    Add MQTT publishing of data

--- go.mod
+++ go.mod
@@ -3,19 +3,23 @@ module github.com/SourceForgery/network-monitor
 go 1.23.3
 
 require (
+	github.com/SourceForgery/duc2mqtt v1.0.8
 	github.com/go-ping/ping v1.1.0
 	github.com/jessevdk/go-flags v1.6.1
 )
 
 require (
 	github.com/rs/zerolog v1.33.0
-	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.5.0 // indirect
 	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	github.com/rotisserie/eris v0.5.4 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"fmt"
+	"github.com/SourceForgery/duc2mqtt/hassio"
 	"github.com/go-ping/ping"
 	"github.com/jessevdk/go-flags"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"net/url"
 	"os"
 	"os/exec"
+	"runtime/debug"
 	"strings"
 	"time"
 )
 
 var opts struct {
-	Interval      int    `long:"interval" description:"Interval in milliseconds" default:"3000"`
-	MaxFailures   int    `long:"max-failures" description:"Maximum number of consecutive failures before running the command" default:"5"`
-	IP            string `long:"host" description:"Host name to ping (IP address recommended)" default:"1.1.1.1"`
-	Timeout       int    `long:"timeout" description:"Timeout in milliseconds" default:"1000"`
-	Unpriviledged bool   `long:"unprivileged" description:"Run ping in unprivileged mode (see https://github.com/go-ping/ping) "`
-	Cooldown      int    `long:"cooldown" description:"Cooldown in milliseconds after running the command before pinging again" default:"300000"`
-	Command       struct {
+	Interval        int    `long:"interval" description:"Interval in milliseconds" default:"3000"`
+	MaxFailures     int    `long:"max-failures" description:"Maximum number of consecutive failures before running the command" default:"5"`
+	IP              string `long:"host" description:"Host name to ping (IP address recommended)" default:"1.1.1.1"`
+	Timeout         int    `long:"timeout" description:"Timeout in milliseconds" default:"1000"`
+	Unprivileged    bool   `long:"unprivileged" description:"Run ping in unprivileged mode (see https://github.com/go-ping/ping) "`
+	Cooldown        int    `long:"cooldown" description:"Cooldown in milliseconds after running the command before pinging again" default:"300000"`
+	MqttUri         string `long:"mqtt-uri" description:"The URI to the mqtt server. If using virtual-host extension (rabbitmq), it would be tcp://test.mosquitto.org:1883/vhost"`
+	MqttUniqueId    string `long:"mqtt-unique-id" description:"The unique id of this device" default:"network-monitor"`
+	MqttName        string `long:"mqtt-name" description:"The name of this device" default:"network-monitor"`
+	MqttTopicPrefix string `long:"mqtt-topic-prefix" description:"The mqtt prefix to use" default:"homeassistant"`
+	Command         struct {
 		Args []string `required:"1"`
 	} `positional-args:"yes"`
 }
+
+var hassioClient *hassio.Client
+var problem *bool
+var hassioSensor = hassio.NewAlarmSensorConfig("internet", "uptime")
 
 func main() {
 	parser := flags.NewParser(&opts, flags.Default)
@@ -45,7 +56,11 @@ func main() {
 	}
 	pinger.Count = 1
 	pinger.Timeout = time.Duration(opts.Timeout) * time.Millisecond
-	pinger.SetPrivileged(!opts.Unpriviledged)
+	pinger.SetPrivileged(!opts.Unprivileged)
+
+	if opts.MqttUri != "" {
+		setupMqtt()
+	}
 
 	failureCount := 0
 	for {
@@ -53,9 +68,11 @@ func main() {
 		if err != nil {
 			failureCount++
 			log.Warn().Err(err).Msgf("Failed to ping %s. Failure count: %d", opts.IP, failureCount)
+			setStatus(false)
 		} else {
 			log.Info().Msgf("Successfully pinged %s", opts.IP)
 			failureCount = 0
+			setStatus(true)
 		}
 
 		if failureCount >= opts.MaxFailures {
@@ -69,15 +86,52 @@ func main() {
 	}
 }
 
+func setupMqtt() {
+	mqttUrl, err := url.Parse(opts.MqttUri)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("Failed to parse mqtt url")
+	}
+
+	amqpVhost := strings.TrimPrefix(mqttUrl.Path, "/")
+	hassioClient, err = hassio.ConnectMqtt(*mqttUrl, amqpVhost, opts.MqttUniqueId, opts.MqttTopicPrefix)
+
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		log.Fatal().Msg("Failed to read build info")
+	}
+
+	hassioClient.Device = &hassio.Device{
+		Identifiers:      []string{opts.MqttUniqueId},
+		Name:             opts.MqttName,
+		SWVersion:        buildInfo.Main.Version,
+		HWVersion:        "N/A",
+		SerialNumber:     "N/A",
+		Model:            "NetworkMonitor",
+		ModelID:          "NetworkMonitor",
+		Manufacturer:     "SourceForgery",
+		ConfigurationURL: fmt.Sprintf("http://localhost/"),
+	}
+	hassioClient.SensorConfigurationData = map[string]hassio.SensorConfig{
+		"internet": hassioSensor,
+	}
+
+	err = hassioClient.SubscribeToHomeAssistantStatus()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to subscribe to Home Assistant status")
+	}
+}
+
 func pingIP(pinger *ping.Pinger) error {
+	oldStats := pinger.Statistics()
 
 	err := pinger.Run()
 	if err != nil {
 		log.Logger.Error().Err(err).Msgf("Failed to ping %s", pinger.Addr())
 	}
 	stats := pinger.Statistics()
+	pinger.Stop()
 
-	if stats.PacketsRecv == 0 {
+	if oldStats.PacketsRecv < stats.PacketsRecv {
 		return fmt.Errorf("no packets received")
 	}
 	return nil
@@ -94,4 +148,23 @@ func runCommand(cmd []string) {
 func terminalOutput() bool {
 	o, _ := os.Stdout.Stat()
 	return (o.Mode() & os.ModeCharDevice) == os.ModeCharDevice
+}
+
+func setStatus(newStatus bool) {
+	if problem != nil && newStatus == *problem {
+		return
+	}
+	problem = &newStatus
+	if hassioClient != nil {
+		value := 1.0
+		if newStatus {
+			value = 0
+		}
+		err := hassioClient.SendSensorData(hassioSensor.SensorType(), map[string]string{
+			hassioSensor.SensorId(): hassioSensor.ConvertValue(value),
+		})
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to send data")
+		}
+	}
 }
